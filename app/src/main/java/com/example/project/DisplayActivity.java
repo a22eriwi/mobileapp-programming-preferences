abefea20d5f2d9af91b4249e0860db04619d58e1
a22eriwi <a22eriwi@student.his.se>
2023-05-09 15:27:28 +0200

    Insåg att jag hade gjort fel så fick göra om en massa saker.

--- app/src/main/java/com/example/project/DisplayActivity.java
+++ app/src/main/java/com/example/project/DisplayActivity.java
@@ -2,13 +2,40 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+    Button Knappen;
+    EditText name;
+    SharedPreferences preferences;
+
+    Button Return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display);
+        setContentView(R.layout.activity_second);
+        name = findViewById(R.id.edit_text);
+        button = findViewById(R.id.save);
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        return = findViewById(R.id.tillback);
+        return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("name", namn.getText().toString());
+            editor.apply();
+        }
+    });
+}
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,8 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private SharedPreferences myPreferenceRef;
-    private SharedPreferences.Editor myPreferenceEditor;
+
+
+    TextView Texten;
+    Button Knappen;
+    SharedPreferences preferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        myPreferenceRef = getPreferences(MODE_PRIVATE);
-        myPreferenceEditor = myPreferenceRef.edit();
-        TextView prefTextRef=new TextView(this);
-        prefTextRef=(TextView)findViewById(R.id.TextView);
-        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
-    }
 
-    public void savePref(View view) {
-        EditText newPrefText=new EditText(this);
-        newPrefText=(EditText)findViewById(R.id.EditText);
-
-        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
-        myPreferenceEditor.apply();
-
-        TextView prefTextRef=new TextView(this);
-        prefTextRef=(TextView)findViewById(R.id.TextView);
-        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
-        newPrefText.setText("");
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        Texten = findViewById(R.id.TextView);
+        Knappen = findViewById(R.id.knappen);
+        Knappen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String name = preferences.getString("name", "no name was found");
+        Texten.setText(name);
+    }
+
 }
